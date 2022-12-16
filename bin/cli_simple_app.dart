e9cbba054d94166d23a6b3ea3a5e5b4a0e9c0267
anadreau <anadreau42@gmail.com>
2022-12-15 21:40:07 -0700

    Added notes to application

--- bin/cli_simple_app.dart
+++ bin/cli_simple_app.dart
@@ -2,17 +2,18 @@ import 'dart:io';
 
 import 'package:cli_simple_app/cli_simple_app.dart';
 
+//Variables for input 1,2 and the method used in the calculation
 double? a;
 double? b;
 int? method;
 
+//enum to track the where in the loop to assign a,b and method
 enum ProcessStep { chooseMethod, inputA, inputB }
-
-enum Method { multiply, divide, add, subtract }
 
 var doLoopStep = ProcessStep.chooseMethod;
 
 void main(List<String> arguments) {
+  //RegExpression to determin if input is a digit or invalid
   var regNum = RegExp(r'(\d+)');
   do {
     if (doLoopStep == ProcessStep.chooseMethod) {
@@ -23,10 +24,10 @@ void main(List<String> arguments) {
       // stdout.writeln('Input second number');
       stdout.write('Input second number: ');
     }
-
+    //Assign input from menu, first number or second number
     final input = stdin.readLineSync();
 
-    //Checks if do loop needs to be exited
+    //Checks if do loop needs to be exited, if true -> Exits program
     if (input == 'exit') {
       break;
 
@@ -44,19 +45,27 @@ void main(List<String> arguments) {
     } else if (regNum.hasMatch(input) == true) {
       //Uses ProcessStep to determine which value to assign
       if (doLoopStep == ProcessStep.chooseMethod && int.parse(input) <= 4) {
+        //Assign input to method
         method = int.parse(input);
+        //Progress doLoopStep so loop will assign inputA next iteration
         doLoopStep = ProcessStep.inputA;
         continue;
       } else if (doLoopStep == ProcessStep.inputA) {
+        //Assign input to a
         a = double.parse(input);
+        //Progress doLoopStep so loop will assign inputB next iteration
         doLoopStep = ProcessStep.inputB;
         continue;
       } else if (doLoopStep == ProcessStep.inputB) {
+        //Assign input to b
         b = double.parse(input);
+        //Takes a, b and method and shows answer to selected method of calculation
         showAnswer(a!, b!, calculate(method!, a, b), method!);
+        //Set a, b, and method back to null incase user goes through menu again
         a = null;
         b = null;
         method = null;
+        //Reset doLoopStep so loop will assign method next iteration
         doLoopStep = ProcessStep.chooseMethod;
       }
     }
